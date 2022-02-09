99c20aa795eab0a4fe56e853b7e870d948306bbd
Rishabh Raj -T (riraj2 - ARICENT TECHNOLOGIES HOLDINGS LIMITED at Cisco) <riraj2@cisco.com>
2022-02-10 00:16:27 +0530

    modifed module

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const indexRouter = require('./routes/index');
 app.set("view engine" , "ejs");
 app.set("views" , __dirname + "/views");
 console.log(__dirname);
-app.set("layout" , "layouts/layout");
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
